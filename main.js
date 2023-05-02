3aa6cfffdca43d5fc0839be3f505d69d85ef58c1
Sebastian Ocando <ocandovivas@gmail.com>
2023-05-01 21:54:02 -0300

    open & close features

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const burguerIcon = document.querySelector('.menu')
@@ -6,35 +5,16 @@ const mobileMenu = document.querySelector('.mobile-menu')
 const cartIcon = document.querySelector('.navbar-shopping-cart')
 const cartList = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
+const productDetail = document.querySelector('#product-detail')
+const closeButton = document.querySelector('.product-detail-close')
+
 const productList = []
-
-
-menuEmail.addEventListener('click', toggleDesktopMenu)
-burguerIcon.addEventListener('click', toggleMobileMenu)
-cartIcon.addEventListener('click', toggleCartList)
-
-function toggleDesktopMenu()  {
-    desktopMenu.classList.toggle('hidden') 
-    cartList.classList.add('hidden')
-}
-
-function toggleMobileMenu()  {
-    mobileMenu.classList.toggle('hidden')
-    cartList.classList.add('hidden')
-}
-
-function toggleCartList()  {
-    cartList.classList.toggle('hidden') 
-    desktopMenu.classList.add('hidden') 
-    mobileMenu.classList.add('hidden')
-}
 
 productList.push({ 
     name: "bike",
     price: 120,
     image: '"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">'
 })
-
 productList.push({ 
     name: "bike",
     price: 120,
@@ -65,7 +45,7 @@ const renderProducts = (arr) => {
     for (product of arr) {
         const htmlCard = `
         <div class="product-card">
-            <img src=${ product.image }
+            <img class="card-img" src=${ product.image }
             <div class="product-info">
                 <div>
                     <p>${ product.price }</p>
@@ -81,5 +61,53 @@ const renderProducts = (arr) => {
 }
 
 renderProducts(productList)
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
+burguerIcon.addEventListener('click', toggleMobileMenu)
+cartIcon.addEventListener('click', toggleCartList)
+closeButton.addEventListener('click', closeProductDetail)
+
+function toggleDesktopMenu()  {
+    desktopMenu.classList.toggle('hidden') 
+    cartList.classList.add('hidden')
+    productDetail.classList.add('hidden')
+}
+
+function toggleMobileMenu()  {
+    mobileMenu.classList.toggle('hidden')
+    cartList.classList.add('hidden')
+    productDetail.classList.add('hidden')
+}
+
+function toggleCartList()  {
+    cartList.classList.toggle('hidden') 
+    desktopMenu.classList.add('hidden') 
+    mobileMenu.classList.add('hidden')
+    productDetail.classList.add('hidden')
+}
+
+function openProductDetail() {
+        productDetail.classList.remove('hidden')
+        cartList.classList.add('hidden')
+        desktopMenu.classList.add('hidden') 
+        mobileMenu.classList.add('hidden')
+}
+
+function closeProductDetail() {
+    productDetail.classList.add('hidden')
+}
+
+const productImage = document.querySelectorAll('.card-img')
+
+const mapImages = () => {
+    for (const product of productImage) {
+        product.addEventListener('click', openProductDetail)
+    }
+}
+
+mapImages()
+
+
+
 
 
